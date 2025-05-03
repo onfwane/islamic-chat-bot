3367f5bd7f5989da157c7794f17e1edf1e8db3c1
onfwane <W.Alhasaseen@gju.edu.jo>
2025-05-03 15:42:20 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import IslamicChatApp from "./IslamicChatApp";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <IslamicChatApp />
-  </React.StrictMode>
-);
